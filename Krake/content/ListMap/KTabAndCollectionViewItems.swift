a8b0a3e8317690eb5bd807696a7e542937e619f9
joel <joel.gerbore@laser-group.com>
2019-06-27 12:20:19 +0200

    KTab modifca per evitare caricamento a primo avvio della home

--- Krake/content/ListMap/KTabAndCollectionViewItems.swift
+++ Krake/content/ListMap/KTabAndCollectionViewItems.swift
@@ -37,13 +37,18 @@ public class KTabAndCollectionViewItems: UIViewController, KTabManagerDelegate {
     override public func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.destination is KItemsCollectionViewController {
             itemsViewController = (segue.destination as! KItemsCollectionViewController)
-            itemsViewController.endPoint = itemsCollectionInfo.endPoint
+            if itemsCollectionInfo.loadBeforeFirstSelection {
+                itemsViewController.endPoint = itemsCollectionInfo.endPoint
+            }
             itemsViewController.extras = itemsCollectionInfo.extras
             itemsViewController.collectionItemsDelegate = itemsViewControllerDelegate
         }
     }
 
     public func tabManager(_ manager: KTabManager, didSelectTermPart termPart: TermPartProtocol?) {
+        if !itemsCollectionInfo.loadBeforeFirstSelection {
+            itemsViewController.endPoint = itemsCollectionInfo.endPoint
+        }
         if let termPart = termPart
         {
             itemsViewController.extras[KParametersKeys.terms] = termPart.identifier!.stringValue
@@ -62,11 +67,14 @@ public class KTabAndCollectionViewItems: UIViewController, KTabManagerDelegate {
 
 public class KItemsCollectionInfo {
     let endPoint: String
+    let loadBeforeFirstSelection: Bool
     let extras: [String : Any]
 
     public init(endPoint: String,
-         extras: [String : Any] = KRequestParameters.parameters(currentPage: 1, pageSize: 15)) {
+         extras: [String : Any] = KRequestParameters.parameters(currentPage: 1, pageSize: 15),
+         loadBeforeFirstSelection: Bool = true) {
         self.endPoint = endPoint
         self.extras = extras
+        self.loadBeforeFirstSelection = loadBeforeFirstSelection
     }
 }
